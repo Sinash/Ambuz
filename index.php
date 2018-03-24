c16b5f9e2bfab7e72ca5b5229e95def7e0aaa719
Sinash Shajahan <shajahan.sinash@gail.com>
2018-03-24 19:29:43 -0400

    update style

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ get_header();
 ?>
 		<div class="details">
 			<span class="name">Sinash Shajahan</span>
-			<span class="contact">shajahan.sinash@gmail.com | (201) 673-5066</span>
-			<span class="links"><a href="https://www.linkedin.com/in/sinashshajahan/">https://www.linkedin.com/in/sinashshajahan/</a></span>
-			<span class="links"><a href="https://github.com/Sinash">https://github.com/Sinash</a></span>
-			<span class="links"><a href="https://www.facebook.com/shajahan.sinash">https://www.facebook.com/shajahan.sinash</a></span>
+			<span class="contact"><a href="mailto:shajahan.sinash@gmail.com" target="_blank">shajahan.sinash@gmail.com</a> | <a href="tel:+1-201-673-5066">201-673-5066</a></span>
+			<span class="links"><a href="https://www.linkedin.com/in/sinashshajahan/" target="_blank">https://www.linkedin.com/in/sinashshajahan/</a></span>
+			<span class="links"><a href="https://github.com/Sinash" target="_blank">https://github.com/Sinash</a></span>
+			<span class="links"><a href="https://www.facebook.com/shajahan.sinash" target="_blank">https://www.facebook.com/shajahan.sinash</a></span>
 		</div>
 		<canvas id="world" width="900" height="600" ><p class="noCanvas">You need a <a href="http://www.google.com/chrome">modern browser</a> to view this.</p></canvas>
 <?php get_footer();
