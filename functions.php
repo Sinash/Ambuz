46696bcb9f8b0332e6e814d299222f853f94d8be
Sinash Shajahan <shajahan.sinash@gail.com>
2018-03-24 20:39:32 -0400

    update style

--- functions.php
+++ functions.php
@@ -11,7 +11,7 @@
  * Enqueue scripts and styles.
  */
 function ambuz_scripts() {
-	$asset_version = '1.2';
+	$asset_version = '2.0';
 	// Add google font.
 	wp_enqueue_style( 'ambus-font', 'https://fonts.googleapis.com/css?family=Roboto:300i,400,700', array(), null );
 
